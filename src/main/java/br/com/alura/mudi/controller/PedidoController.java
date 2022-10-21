515b27f4936144fafa7df41e1db21d77fd0c03fb
laurodrigues <laurodrigues29@gmail.com>
2022-10-21 09:47:13 -0300

    controller do pedido, validacao e primeiras views

---
+++ src/main/java/br/com/alura/mudi/controller/PedidoController.java
@@ -0,0 +1,38 @@
+package br.com.alura.mudi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mudi.modelo.Pedido;
+import br.com.alura.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("/pedido")
+public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("formulario")
+	public String formulario(RequisicaoNovoPedido requisicaoNovoPedido) {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicaoNovoPedido, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = requisicaoNovoPedido.toPedido();
+		pedidoRepository.save(pedido);
+		return "pedido/formulario";
+	}
+}
---
+++ src/main/java/br/com/alura/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,53 @@
+package br.com.alura.mudi.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.alura.mudi.modelo.Pedido;
+
+public class RequisicaoNovoPedido {
+	@NotBlank
+	private String nomeProduto;
+	@NotBlank
+	private String urlDaImagem;
+	@NotBlank
+	private String urlDoProduto;
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlDaImagem() {
+		return urlDaImagem;
+	}
+	public void setUrlDaImagem(String urlDaImagem) {
+		this.urlDaImagem = urlDaImagem;
+	}
+	public String getUrlDoProduto() {
+		return urlDoProduto;
+	}
+	public void setUrlDoProduto(String urlDoProduto) {
+		this.urlDoProduto = urlDoProduto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setDescricao(descricao);
+		pedido.setUrlDaImagem(urlDaImagem);
+		pedido.setUrlDoProduto(urlDoProduto);
+		pedido.setNomeProduto(nomeProduto);
+		
+		return pedido;
+		
+	}
+	
+}
